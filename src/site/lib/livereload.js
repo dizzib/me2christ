c0508bbc889eb53c1a161515c3dd69e830555c51
dizzib <dizzib.com@gmail.com>
2024-10-02 14:04:09 +0100

    fmt

--- src/site/lib/livereload.js
+++ src/site/lib/livereload.js
@@ -1,1 +1,3 @@
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+document.write(
+  '<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>'
+)
