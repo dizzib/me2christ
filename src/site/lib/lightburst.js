ac078fbb5b395c300eaf83c66d92b7aae08c3bab
dizzib <dizzib.com@gmail.com>
2024-10-08 20:29:09 +0100

    add consts

--- src/site/lib/lightburst.js
+++ src/site/lib/lightburst.js
@@ -1,9 +1,11 @@
 addEventListener("DOMContentLoaded", () => {
-  b = document.body
-  d = document.documentElement
-  l = b.querySelector('.light')
-  i = b.querySelector('.intro')
-  o = b.querySelector('.outro')
+  const b = document.body
+  const d = document.documentElement
+  const l = b.querySelector('.light')
+  const i = b.querySelector('.intro')
+  const o = b.querySelector('.outro')
+
+  var MAP_ALL
 
   function burst() {
     SCALE_MAX = 5 // max lightburst scale
@@ -13,21 +15,23 @@ addEventListener("DOMContentLoaded", () => {
   }
 
   function refresh_height() {
-    intro_height = i.clientHeight
-    outro_height = o.clientHeight
-    total_height = (d.scrollHeight || b.scrollHeight) - d.clientHeight
+    const intro_height = i.clientHeight
+    const outro_height = o.clientHeight
+    const total_height = (d.scrollHeight || b.scrollHeight) - window.innerHeight
 
-    LEN_MAP = total_height // divide full height into this many units
-    LEN_RAMP = 400 // controls light to/from dark transition speed
-    LEN_INTRO = intro_height - LEN_RAMP
-    LEN_OUTRO = outro_height - LEN_RAMP
-    LEN_MAIN = LEN_MAP - LEN_INTRO - LEN_OUTRO - (LEN_RAMP * 2)
-    MAP_INTRO = Array(LEN_INTRO).fill(0)
-    MAP_UP = Array(LEN_RAMP).fill('').map((_, i) => i / LEN_RAMP)
-    MAP_MAIN = Array(LEN_MAIN).fill(1)
-    MAP_DOWN = [...MAP_UP].reverse()
-    MAP_OUTRO = Array(LEN_OUTRO).fill(0)
-    MAP_ALL = [...MAP_INTRO, ...MAP_UP, ...MAP_MAIN, ...MAP_DOWN, ...MAP_OUTRO] // array of lightburst intensity at each scroll position, 0 = dark, 1 = light
+    const LEN_MAP = total_height // divide full height into this many units
+    const LEN_RAMP = 400 // controls light to/from dark transition speed
+    const LEN_INTRO = intro_height - LEN_RAMP
+    const LEN_OUTRO = outro_height - LEN_RAMP
+    const LEN_MAIN = LEN_MAP - LEN_INTRO - LEN_OUTRO - (LEN_RAMP * 2)
+    const MAP_INTRO = Array(LEN_INTRO).fill(0)
+    const MAP_UP = Array(LEN_RAMP).fill('').map((_, i) => i / LEN_RAMP)
+    const MAP_MAIN = Array(LEN_MAIN).fill(1)
+    const MAP_DOWN = [...MAP_UP].reverse()
+    const MAP_OUTRO = Array(LEN_OUTRO).fill(0)
+
+    // array of lightburst intensity at each scroll position, 0 = dark, 1 = light
+    MAP_ALL = [...MAP_INTRO, ...MAP_UP, ...MAP_MAIN, ...MAP_DOWN, ...MAP_OUTRO]
   }
 
   // event handlers
