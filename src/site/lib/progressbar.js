c3f0600e6e409e6c659f5a47bbeb042ba796bc0a
dizzib <dizzib.com@gmail.com>
2024-10-08 22:20:16 +0100

    refactor

--- src/site/lib/progressbar.js
+++ src/site/lib/progressbar.js
@@ -1,19 +1,22 @@
 addEventListener("DOMContentLoaded", () => {
-  b = document.body
-  d = document.documentElement
-  p = b.querySelector('.progressbar')
+  const B = document.body
+  const D = document.documentElement
+  const P = B.querySelector('.progressbar')
+
+  var total_height
 
   function progress() {
-    scrollpos = Math.round(d.scrollTop || b.scrollTop)
-    percent = scrollpos / total_height * 100
-    p.style.width = percent + '%'
+    const SCROLLPOS = Math.round(D.scrollTop || B.scrollTop)
+    var percent = SCROLLPOS / total_height * 100
+    P.style.width = percent + '%'
   }
 
   function refresh_height() {
-    total_height = (d.scrollHeight || b.scrollHeight) - window.innerHeight - 50
+    total_height = (D.scrollHeight || B.scrollHeight) - window.innerHeight - 50
   }
 
   // event handlers
+  window.addEventListener('orientationchange', refresh_height)
   window.addEventListener('resize', refresh_height)
   window.addEventListener('scroll', progress)
 
